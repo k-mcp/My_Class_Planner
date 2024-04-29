60817b62cf2100f61c557d4545bea39a1cc320d0
kenryu <jryu1521@gmail.com>
2024-04-29 10:32:08 +0900

    mail sender added

---
+++ src/main/java/mcp/myclassplanner/model/dto/MailTO.java
@@ -0,0 +1,31 @@
+package mcp.myclassplanner.model.dto;
+
+public class MailTO {
+    private String address;
+    private String title;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
