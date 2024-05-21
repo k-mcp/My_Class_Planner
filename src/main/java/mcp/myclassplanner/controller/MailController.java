6e9db630142d83d09a521339dde5b7122867a0dc
kenryu <jryu1521@gmail.com>
2024-05-21 15:40:31 +0900

    password reset

--- src/main/java/mcp/myclassplanner/controller/MailController.java
+++ src/main/java/mcp/myclassplanner/controller/MailController.java
@@ -8,12 +8,12 @@ import mcp.myclassplanner.model.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/mail")
 public class MailController {
     private final MailService mailService;
     private final MemberService memberService;
@@ -23,7 +23,7 @@ public class MailController {
         this.mailService = mailService;
         this.memberService = memberService;
     }
-    @GetMapping("/send")
+    @GetMapping("/mail/send")
     public ModelAndView sendTestMail(String email, ModelAndView mv) {
 
         String key = memberService.getAuthCode(email);
@@ -45,5 +45,26 @@ public class MailController {
         mv.setViewName("redirect:/loading");
         return mv;
     }
+    @PostMapping("/auth/forgotpassword")
+    public ModelAndView forgotPassword(String email, ModelAndView mv) {
 
+        String key = memberService.getAuthCode(email);
+        System.out.println("key = " + key);
+        MailTO mailTO = new MailTO();
+
+        mailTO.setAddress(email);
+        mailTO.setTitle("My Class Planner confirmation email");
+        mailTO.setMessage(new StringBuffer().append("<h1>[비밀번호 재설정]</h1>")
+                .append("<p>아래 링크를 클릭하세요.</p>")
+                .append("<a href='http://localhost:8081/auth/forgotpassword?email=")
+                .append(email)
+                .append("&authKey=")
+                .append(key)
+                .append("' target='_blenk'>비밀번호 재설정</a>")
+                .toString());
+
+        mailService.sendMail(mailTO);
+        mv.setViewName("redirect:/loading");
+        return mv;
+    }
 }
