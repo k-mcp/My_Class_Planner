69a29eb1428fc67414a511304b39ae3ab9c5137f
kenryu <jryu1521@gmail.com>
2024-05-28 12:29:21 +0900

    username query fixed

--- src/main/java/mcp/myclassplanner/controller/SearchUserController.java
+++ src/main/java/mcp/myclassplanner/controller/SearchUserController.java
@@ -56,9 +56,10 @@ public class SearchUserController {
 
     @PostMapping(value="search", produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public String memberExist(@RequestBody String query) throws JsonProcessingException {
-            query = query.replace("\"","").replace("}","").substring(query.indexOf(":")-1);
-        List<String> usernames = memberService.searchAllMember(query);
+    public String memberExist(@RequestBody Map<String,Object> query) throws JsonProcessingException {
+        String username = (String) query.get("query");
+        List<String> usernames = memberService.searchAllMember(username);
+
         ObjectMapper mapper = new ObjectMapper();
 
         return mapper.writeValueAsString(usernames);
